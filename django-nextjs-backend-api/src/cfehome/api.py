843fd1724909efa76256c753e3767b8b87c799db
unknown <joshuajulius2030@gmail.com>
2025-08-01 14:52:59 +0100

    django hello world

--- django-nextjs-backend-api/src/cfehome/api.py
+++ django-nextjs-backend-api/src/cfehome/api.py
@@ -1,9 +1,20 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
 
-@api.get("/hello")
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    # is not request.is_authenticated
+    email: str = None
 
+
+
+@api.get("/hello")
 def hello(request):
     print(request)
     return("Hello world!")  
+
+@api.get("me", response=UserSchema)
+def me(request):
+    return request.user
