a2cec2b0c265fadbc7b3c41307677f60b5f48366
unknown <joshuajulius2030@gmail.com>
2025-08-19 01:14:07 +0100

    solved cors error issues, installed and setup cors headers

--- django-nextjs-backend-api/src/cfehome/settings.py
+++ django-nextjs-backend-api/src/cfehome/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third party apps
+    'corsheaders',
     'ninja_jwt',
     'ninja_extra',
     # internal
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,6 +57,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cfehome.urls'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://esample.com",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+
+]
 
 TEMPLATES = [
     {
