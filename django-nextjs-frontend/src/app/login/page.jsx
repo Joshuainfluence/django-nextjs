5565690615981d17ea0b51071dd65a75bf5ea56d
unknown <joshuajulius2030@gmail.com>
2025-08-20 05:11:31 +0100

    post form api

---
+++ django-nextjs-frontend/src/app/login/page.jsx
@@ -0,0 +1,38 @@
+"use client"
+// -> url -> /login
+const LOGIN_URL = "http://127.0.0.1:8000/api/token/pair"
+export default function Page() {
+
+   async function handleSubmit(event) {
+        event.preventDefault()
+        console.log(event, event.target)
+
+        const formData = new FormData(event.target)
+        const objectFromForm = Object.fromEntries(formData)
+        const jsonData = JSON.stringify(objectFromForm)
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: jsonData
+        }
+
+        const response = await fetch(LOGIN_URL, requestOptions) 
+        const rData = response.json()
+        console.log(rData)
+        if(response.ok){
+            console.log("Logged in")
+        }
+    }
+    return <div className="h-[95vh]">
+        <div className="py-5 max-w-md mx-auto">
+            <h1> Login here</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="username" placeholder="Your Username" />
+                <input type="password" name="password" placeholder="Your Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+}
